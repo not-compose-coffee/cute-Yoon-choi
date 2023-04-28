e132a7a255c95fcc49f261d7302229674bd0c164
yoon <cbj0010@naver.com>
2023-04-28 12:40:31 +0900

    [feat/composeArticle] ui 넣기

--- week1/practice/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ week1/practice/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,46 +3,56 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent { DefaultPreview() }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
-
-@Composable
-private fun ArticleCard(
-    title: String,
-    shortDescription: String,
-    longDescription: String,
-    imagePainter: Painter,
-    modifier: Modifier = Modifier,
-) {
-    Column() { }
+fun ComposeArticleApp() {
 }
 
+@Composable
+private fun ArticleCard(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            stringResource(R.string.title_jetpack_compose_tutorial),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Spacer(modifier = Modifier.fillMaxHeight(0.1f))
+        Text(
+            stringResource(R.string.compose_short_desc),
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp), textAlign = TextAlign.Justify
+        )
+        Spacer(modifier = Modifier.fillMaxHeight(0.1f))
+        Text(
+            stringResource(R.string.compose_long_desc),
+            Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ArticleCard()
+}
