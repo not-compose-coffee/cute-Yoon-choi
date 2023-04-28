c16cf2b4a537361df9aba9866de859634cbd1832
yoon <cbj0010@naver.com>
2023-04-28 11:58:08 +0900

    [feat/diceRoll] random함수 추가

--- week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ week1/practice/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,18 +3,21 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,15 +38,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    modifier.fillMaxSize()
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+    modifier.fillMaxSize()  //이미지가 구성할 수 있는 부분을 꽉 채우도록
     modifier.wrapContentSize(Alignment.Center)
+    val result = remember { mutableStateOf(1) }
+    val imageResource = when (result.value) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { /*TODO*/ }) {
+        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result.value = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
     }
-
 }
 
 @Preview(showBackground = true)
